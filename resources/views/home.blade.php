3197cd11530d74dd14032d249cd6a66acfbc5735
Ingeniero Fernando <fernando@bitquark.com.mx>
2020-08-29 19:56:29 -0500

    Corrección de errores

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,72 +1,10 @@
-<!DOCTYPE html>
-<html lang="es_MX">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta http-equiv="Content-Language" content="es-mx" />
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>@yield("title")</title>
-        <link rel="stylesheet" type="text/css" href="{{mix('/css/app.css')}}">
-        <script src="{{ mix('/js/app.js') }}"></script>
+@extends('layouts.app')
 
-        {{-- SEO TAGS --}}
-        <meta
-            name="description"
-            content="{{@yield('seo_description')}}"
-        />
-        <meta
-            name="keywords"
-            content="Blog, noticias del momento, últimas noticias, código abierto, open source"
-        />
-        <meta name="robots" content="index,follow" />
-        <meta property="og:locale" content="es_MX" />
-        <meta property="og:type" content="website" />
-        <meta
-            property="og:title"
-            content="{{@yield('title')}}"
-        />
-        <meta
-            property="og:description"
-            content="{{@yield('seo_description')}}"
-        />
-        <meta property="og:url" content="http://localhost:8080" />
-        <meta
-            property="og:site_name"
-            content="Open Blog"
-        />
-        <meta
-            property="og:image"
-            content="{{@yield('seo_image')}}"
-        />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta
-            name="twitter:description"
-            content="{{@yield('seo_description')}}"
-        />
-        <meta
-            name="twitter:title"
-            content="{{@yield('title')}}"
-        />
+{{-- Etiquetas SEO --}}
 
-        <meta
-            name="twitter:image"
-            content="{{@yield('seo_image')}}"
-        />
-    </head>
-    <body>
-        @yield("navigation")
-        <main>
-            {{--
-                El yield content cubre la mayoría de elementos dentro,
-                del mismo, existirá la etiqueta `article`, lo que nos
-                permitirá mejorar la posición SEO de nuestro blog.
-            --}}
-            @yield("content")
-        </main>
-        @yield("footer")
-        <!--
-            Bitquark software® - 2020
-            The Open Blog Proyect
-        -->
-    </body>
-</html>
+{{-- Navbar --}}
+
+{{-- Contenido de la página principal --}}
+@section('content')
+<h1>Hola</h1>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,80 +1,73 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
+<html lang="es_MX">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Content-Language" content="es-mx" />
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>@yield("title")</title>
+        <link rel="stylesheet" type="text/css" href="{{mix('/css/app.css')}}">
+        {{-- SEO TAGS --}}
+        <meta
+            name="description"
+            content="@yield('seo_description')"
+        />
+        <meta
+            name="keywords"
+            content="Blog, noticias del momento, últimas noticias, código abierto, open source"
+        />
+        <meta name="robots" content="index,follow" />
+        <meta property="og:locale" content="es_MX" />
+        <meta property="og:type" content="website" />
+        <meta
+            property="og:title"
+            content="@yield('title')"
+        />
+        <meta
+            property="og:description"
+            content="@yield('seo_description')"
+        />
+        <meta property="og:url" content="http://localhost:8080" />
+        <meta
+            property="og:site_name"
+            content="Open Blog"
+        />
+        <meta
+            property="og:image"
+            content="@yield('seo_image')"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+            name="twitter:description"
+            content="@yield('seo_description')"
+        />
+        <meta
+            name="twitter:title"
+            content="@yield('title')"
+        />
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
-</body>
+        <meta
+            name="twitter:image"
+            content="@yield('seo_image')"
+        />
+    </head>
+    <body>
+        <div id="app">
+            @yield("navigation")
+            <main id="app">
+                {{--
+                    El yield content cubre la mayoría de elementos dentro,
+                    del mismo, existirá la etiqueta `article`, lo que nos
+                    permitirá mejorar la posición SEO de nuestro blog.
+                --}}
+                @yield("content")
+            </main>
+            @yield("footer")
+        </div>
+        <script src="{{ mix('/js/app.js') }}" defer async></script>
+        <!--
+            Bitquark software® - 2020
+            The Open Blog Proyect
+        -->
+    </body>
 </html>
